e2a223a333fd2e1472f223bdb24084963c9c56f9
BadAtComputers <34315695+AxiomInfinite@users.noreply.github.com>
2021-10-19 09:59:23 -0500

    Add files via upload

--- RestAPI.py
+++ RestAPI.py
@@ -1,24 +1,27 @@
 #Step 1 How to get the data from https://inspirehep.net/
+import os, sys
 import requests
 from pprint import pprint
 
-
+#Find a way to return this point when error hits
 record_type = input("What Section would you like to search?")
-query_string = input("query bullshit")
+query_string = input("")
+#Ask user toe search through list 
 
 #create a function to search through the website
 def lookup(record_type, query_string):
-    #Types in Search area
     
     #require record_type to equal one of the words from list
     list = ['literature', 'authors', 'conferences', 'seminars', 'journals', 'jobs', 'experiments', 'data']
     if not record_type in list:
-        print("Error, this is an incorrect search parameter. ")
-        #stop action if search parameter is wrong
-
+        print("Error, this is an incorrect search parameter, try again. ")
+        #Figure out how to restart Function if this error
+        quit()
+        
     url = f"https://inspirehep.net/api/{record_type}?q={query_string}"
     r = requests.get(url)
     return r.content
-#set lookup = to a variable
+
+#set lookup = to a variable for more flexibilty
 api_results = lookup(record_type,query_string)
 pprint(lookup(record_type,query_string))
